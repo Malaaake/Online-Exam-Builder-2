8e66d51dc6292987d484c6566ddb7f1b6812daa4
Malaaake <108901288+Malaaake@users.noreply.github.com>
2024-09-24 00:13:07 +0100

    Update Answer.java

--- Online-Exam-Builder/src/main/java/pfe/exambuilder/model/Answer.java
+++ Online-Exam-Builder/src/main/java/pfe/exambuilder/model/Answer.java
@@ -1,5 +1,7 @@
 package pfe.exambuilder.model;
 
+import java.time.LocalDateTime;
+
 import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
 
 import jakarta.persistence.Entity;
@@ -15,16 +17,50 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class Answer {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String content;
-
+    private LocalDateTime createdAt;
     @ManyToOne
     private Question question;
+    private String author;
 
-    private Boolean isCorrect;
+    public Question getQuestion() {
+        return question;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
