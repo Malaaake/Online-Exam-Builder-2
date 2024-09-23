ae77ce5c5c3d22f23003dec94522dcf65b78e74f
Malaaake <108901288+Malaaake@users.noreply.github.com>
2024-09-24 00:14:11 +0100

    Update AnswerController.java

--- Online-Exam-Builder/src/main/java/pfe/exambuilder/controller/AnswerController.java
+++ Online-Exam-Builder/src/main/java/pfe/exambuilder/controller/AnswerController.java
@@ -1,6 +1,10 @@
 package pfe.exambuilder.controller;
 
+import java.time.LocalDateTime;
+
+import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import pfe.exambuilder.dto.AnswerDto;
 import pfe.exambuilder.model.Answer;
+import pfe.exambuilder.repository.QuestionRepository;
 import pfe.exambuilder.service.AnswerService;
 
 
@@ -16,14 +21,52 @@ import pfe.exambuilder.service.AnswerService;
 @RestController
 @RequestMapping("/api/answers")
 public class AnswerController {
-
     @Autowired
     private AnswerService answerService;
 
+    @Autowired
+    private QuestionRepository questionRepository;
+
     @PostMapping
     public ResponseEntity<AnswerDto> createAnswer(@RequestBody AnswerDto answerDto) {
+        try {
+            Answer answer = convertToEntity(answerDto);
+            Answer savedAnswer = answerService.saveAnswer(answer);
+            AnswerDto savedAnswerDto = convertToDto(savedAnswer);
+            return ResponseEntity.ok(savedAnswerDto);
+        } catch (QuestionNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    private Answer convertToEntity(AnswerDto dto) throws QuestionNotFoundException {
         Answer answer = new Answer();
-        Answer savedAnswer = answerService.saveAnswer(answer);
-        return ResponseEntity.ok(answerDto);
+        answer.setContent(dto.getContent());
+        answer.setAuthor(dto.getAuthor());
+        answer.setCreatedAt(LocalDateTime.now());
+        
+        Question question = questionRepository.findById(dto.getQuestionId())
+            .orElseThrow(() -> new QuestionNotFoundException("Question not found with id: " + dto.getQuestionId()));
+        answer.setQuestion(question);
+        
+        return answer;
+    }
+
+    private AnswerDto convertToDto(Answer entity) {
+        AnswerDto dto = new AnswerDto();
+        dto.setId(entity.getId());
+        dto.setContent(entity.getContent());
+        dto.setAuthor(entity.getAuthor());
+        dto.setCreatedAt(entity.getCreatedAt());
+        dto.setQuestionId(entity.getQuestion().getId());
+        return dto;
+    }
+}
+
+class QuestionNotFoundException extends Exception {
+    public QuestionNotFoundException(String message) {
+        super(message);
     }
 }
