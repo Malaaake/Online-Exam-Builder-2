dc8a7c0aa8ce550b5226d34466192afc5b290ca7
Malaaake <108901288+Malaaake@users.noreply.github.com>
2024-09-24 01:39:50 +0100

    Update AnswerDto.java

--- Online-Exam-Builder/src/main/java/pfe/exambuilder/dto/AnswerDto.java
+++ Online-Exam-Builder/src/main/java/pfe/exambuilder/dto/AnswerDto.java
@@ -1,5 +1,7 @@
 package pfe.exambuilder.dto;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,13 +12,49 @@ import lombok.NoArgsConstructor;
 public class AnswerDto {
     private String content;
     private Boolean isCorrect;
+    private Long id;
+  
+    private String author;
+    private LocalDateTime createdAt;
+    private Long questionId;
     
-    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getContent() {
         return content;
     }
 
-    public Boolean getIsCorrect() {
-        return isCorrect;
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
     }
 }
